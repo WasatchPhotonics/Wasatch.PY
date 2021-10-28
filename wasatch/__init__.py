34b78dddcdd4a56c340301480a4d4eaa8fcb5eb3
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2021-10-28 12:09:43 -0400

    add comment

--- wasatch/__init__.py
+++ wasatch/__init__.py
@@ -1,3 +1,3 @@
 """This package is a driver for Wasatch Photonics devices"""
-__version__ = "1.1.36"
+__version__ = "1.1.36" #This is used by flit and other pypi things
 version = "1.1.36" # This is to avoid breaking other files that originally used .version instead of __version__
