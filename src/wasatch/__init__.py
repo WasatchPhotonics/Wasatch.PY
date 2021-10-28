1a317f7679e1099207cde4a8de6803d6f529fd53
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2021-10-28 11:36:41 -0400

    moving wasatch out of src directory

--- src/wasatch/__init__.py
+++
@@ -1,3 +0,0 @@
-"""This package is a driver for Wasatch Photonics devices"""
-__version__ = "1.1.35"
-version = "1.1.35" # This is to avoid breaking other files that originally used .version instead of __version__
---
+++ wasatch/__init__.py
@@ -0,0 +1,3 @@
+"""This package is a driver for Wasatch Photonics devices"""
+__version__ = "1.1.36"
+version = "1.1.36" # This is to avoid breaking other files that originally used .version instead of __version__
