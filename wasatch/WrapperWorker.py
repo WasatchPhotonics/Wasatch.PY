7c114441416fd60dc631688859db5d21ae282aca
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2022-04-21 10:42:11 -0400

    remove hard coded value

--- wasatch/WrapperWorker.py
+++ wasatch/WrapperWorker.py
@@ -78,7 +78,7 @@ class WrapperWorker(threading.Thread):
                         message_queue = self.message_queue)
             else:
                 log.debug("instantiating WasatchDevice")
-                self.connected_device = device_classes[3](
+                self.connected_device = device_classes[device_classes.index(WasatchDevice)](
                     device_id = self.device_id,
                     message_queue = self.message_queue)
         except:
