52a69d37aeac7a42597548879cdd6a58bce134dc
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2022-04-27 12:20:32 -0400

    eeprom bug with slice appears fixed

--- wasatch/SPIDevice.py
+++ wasatch/SPIDevice.py
@@ -56,6 +56,25 @@ class SPIDevice(InterfaceDevice):
     """
 
     INTEGRATION_ADDRESS = 0x11
+    CRC_8_TABLE = [
+      0, 94,188,226, 97, 63,221,131,194,156,126, 32,163,253, 31, 65,
+    157,195, 33,127,252,162, 64, 30, 95,  1,227,189, 62, 96,130,220,
+     35,125,159,193, 66, 28,254,160,225,191, 93,  3,128,222, 60, 98,
+    190,224,  2, 92,223,129, 99, 61,124, 34,192,158, 29, 67,161,255,
+     70, 24,250,164, 39,121,155,197,132,218, 56,102,229,187, 89,  7,
+    219,133,103, 57,186,228,  6, 88, 25, 71,165,251,120, 38,196,154,
+    101, 59,217,135,  4, 90,184,230,167,249, 27, 69,198,152,122, 36,
+    248,166, 68, 26,153,199, 37,123, 58,100,134,216, 91,  5,231,185,
+    140,210, 48,110,237,179, 81, 15, 78, 16,242,172, 47,113,147,205,
+     17, 79,173,243,112, 46,204,146,211,141,111, 49,178,236, 14, 80,
+    175,241, 19, 77,206,144,114, 44,109, 51,209,143, 12, 82,176,238,
+     50,108,142,208, 83, 13,239,177,240,174, 76, 18,145,207, 45,115,
+    202,148,118, 40,171,245, 23, 73,  8, 86,180,234,105, 55,213,139,
+     87,  9,235,181, 54,104,138,212,149,203, 41,119,244,170, 72, 22,
+    233,183, 85, 11,136,214, 52,106, 43,117,151,201, 74, 20,246,168,
+    116, 42,200,150, 21, 75,169,247,182,232, 10, 84,215,137,107, 53
+    ]
+
 
     def __init__(self, device_id: DeviceID, message_queue: Queue) -> None:
         # if passed a string representation of a DeviceID, deserialize it
@@ -128,9 +147,7 @@ class SPIDevice(InterfaceDevice):
             while self.ready.value:
                 self.SPI.readinto(response,0,2)
             page = self._EEPROMReadPage(i)
-            log.info(f"spi read page {i} with data {page}")
             eeprom_pages.append(page)
-        eeprom_pages = [bytearray([val for val in page[9:74]]) for page in eeprom_pages]
         self.settings.eeprom.parse(eeprom_pages)
         self.settings.eeprom.active_pixels_horizontal = 1952
         self.settings.state.integration_time_ms = 10
@@ -218,7 +235,7 @@ class SPIDevice(InterfaceDevice):
 
     def _EEPROMReadPage(self, page: int) -> list[bytes]:
         while True:
-            EEPROMPage  = bytearray(74)
+            EEPROMPage  = bytearray(75)
             command     = bytearray(7)
             command     = [0x3C, 0x00, 0x02, 0xB0, (0x40 + page), 0xFF, 0x3E]
             self.SPI.write(command, 0, 7)
@@ -227,12 +244,14 @@ class SPIDevice(InterfaceDevice):
             self.SPI.write_readinto(command, EEPROMPage)
             log.debug(f"read eeprom page of {EEPROMPage}")
             log.info(f"checking if busy")
-            log.info(f"EEPROMPage cmd is {EEPROMPage[:6]}")
-            log.info(f"equals 6 0x3 is {EEPROMPage[:6] == [0x3,0x3,0x3,0x3,0x3,0x3]}")
-            if EEPROMPage[:6] == [0x3,0x3,0x3,0x3,0x3,0x3]:
+            log.info(f"EEPROMPage cmd is {EEPROMPage[5]}")
+            log.info(f"equals 5 0x3 is {EEPROMPage[5] == 0x3}")
+            if EEPROMPage[5] == 0x3:
                 log.info(f"trying to read page {page}, got response status of {EEPROMPage[:6]}")
                 continue
             else:
+                EEPROMPage = EEPROMPage[EEPROMPage.index(b'<')+4:len(EEPROMPage)-1]
+                log.info(f"sliced page value is {EEPROMPage}")
                 break
         self._SPIBusy()
         log.info(f"for page {page} got values {EEPROMPage}")
