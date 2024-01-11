c95a61a1885a15f094fc0730d6b7b2f5e29d5f03
Mark Zieg <mzieg@wasatchphotonics.com>
2024-01-11 16:09:38 -0500

    gave ProcessedReading .cropped and .interpolated recursive elements

--- wasatch/ProcessedReading.py
+++ wasatch/ProcessedReading.py
@@ -1,9 +1,11 @@
 import logging
 import numpy as np
+from copy import copy
 
 from wasatch import utils as wasatch_utils
 
 log = logging.getLogger(__name__)
+
 
 ##
 # This class encapsulates anything done to a wasatch.Reading after it has been
@@ -25,34 +27,46 @@ log = logging.getLogger(__name__)
 # a Reading attribute.  (Also, the loaded Measurement should have a 
 # source_pathname attribute.)
 #
-class ProcessedReading(object):
+# @see ORDER_OF_OPERATIONS.md in ENLIGHTEN/docs 
+#
+class ProcessedReading:
 
     session_count = 0
 
     def clear(self):
         self.reading = None
         self.device_id = None
-        self.raw = None
+
         self.processed = None
-        self.processed_cropped = None
-        self.reference = None
+        self.raw = None
         self.dark = None
+        self.reference = None
+        self.wavelengths = None
+        self.wavenumbers = None
+
+        self.settings = None
+
         self.dark_corrected = False
+        self.raman_intensity_corrected = False
+        self.used_reference = False
+        self.deconvolved = False
+
         self.recordable_reference = None
         self.recordable_dark = None
-        self.used_reference = False
+
         self.declared_match = None
-        self.raman_intensity_corrected = False
-        self.deconvolved = False
-        self.wavelengths_cropped = None
-        self.wavenumbers_cropped = None
-        self.first_pixel = None
-        self.plugin_metadata = None # extra data added by Plugin
+        self.first_pixel = None             # MZ: ?
+        self.plugin_metadata = None
+
+        # these are optional handles to secondary ProcessedReadings
+        # (just proc/raw/dark/ref/wl/wn, no further recursion)
+        self.cropped = None
+        self.interpolated = None
 
     ##
     # @param d (Input) if instantiating from a dict (External API or loaded JSON),
     #                  this is the dictionary containing parsed values
-    def __init__(self, reading=None, d=None):
+    def __init__(self, reading=None, d=None, settings=None):
 
         self.clear()
     
@@ -101,38 +115,80 @@ class ProcessedReading(object):
         self.session_count = ProcessedReading.session_count
         ProcessedReading.session_count += 1
 
+        if settings:
+            self.settings = settings
+            if settings.wavelengths is not None:
+                self.wavelengths = copy(settings.wavelengths)
+            if settings.wavenumbers is not None:
+                self.wavenumbers = copy(settings.wavenumbers)
+
         if d is not None:
             self.load_from_dict(d)
 
-    def has_dark(self): # -> bool 
+    def get_pixel_count(self):
+        spectrum = self.get_processed()
+        if spectrum is not None:
+            return len(spectrum)
+        return 0
+
+    def has_dark(self): 
         return self.dark is not None
 
-    def has_reference(self): # -> bool 
+    def has_reference(self): 
         return self.reference is not None
 
-    def is_cropped(self): # -> bool 
-        return self.processed_cropped is not None and len(self.processed_cropped) > 0
+    def is_cropped(self): 
+        return self.cropped is not None 
 
-    def has_processed(self): # -> bool 
-        return self.processed is not None or self.processed_cropped is not None
+    def has_processed(self): 
+        return self.processed is not None 
 
-    def get_processed(self):
-        if self.processed_cropped is None:
-            return self.processed
-        else:
-            return self.processed_cropped
+    def _get_array(self, name, stage):
+        sources = [self.interpolated, self.cropped, self]
+        if stage:
+            stage = stage.lower()
+            if stage == "cropped":
+                sources = [self.cropped, self]
+            elif stage == "orig":
+                sources = [self]
 
+        for obj in sources:
+            if obj is not None:
+                if hasattr(obj, name):
+                    v = getattr(obj, name)
+                    if v is not None:
+                        return v
+
+    def get_processed(self, stage=None):
+        return self._get_array("processed", stage)
+    def get_raw(self, stage=None):
+        return self._get_array("raw", stage)
+    def get_dark(self, stage=None):
+        return self._get_array("dark", stage)
+    def get_reference(self, stage=None):
+        return self._get_array("reference", stage)
+    def get_wavelengths(self, stage=None):
+        return self._get_array("wavelengths", stage)
+    def get_wavenumbers(self, stage=None):
+        return self._get_array("wavenumbers", stage)
+
+    # MZ: who calls this?
     def set_processed(self, spectrum):
-        if self.is_cropped():
+        if self.cropped:
             log.debug("set_processed: updating cropped")
-            self.processed_cropped = spectrum
+            self.cropped.processed = spectrum
         else:
             log.debug("set_processed: updating non-cropped")
             self.processed = spectrum
 
+    # should be done before .cropped created
     def correct_dark(self, dark):
         if self.dark_corrected:
             log.debug("already dark-corrected")
+            return
+
+        if self.cropped and len(self.cropped.processed) != len(dark):
+            log.error(f"correct_dark: len(cropped.processed) {len(self.cropped.processed)} != len(dark) {len(dark)}")
             return
 
         if dark is None:
@@ -175,7 +231,17 @@ class ProcessedReading(object):
         log.info("  Reference:            %s", None if self.reference            is None else self.reference[:n])
         log.info("  Recordable Dark:      %s", None if self.recordable_dark      is None else self.recordable_dark[:n])
         log.info("  Recordable Reference: %s", None if self.recordable_reference is None else self.recordable_reference[:n])
+        log.info("  Wavelengths:          %s", None if self.wavelengths          is None else self.wavelengths[:n])
+        log.info("  Wavenumbers:          %s", None if self.wavenumbers          is None else self.wavenumbers[:n])
+        log.info("  Pixels:               %s", None if self.wavelengths          is None else len(self.wavelengths))
+        if self.cropped:
+            log.info("  Cropped:")
+            self.cropped.dump()
+        if self.interpolated:
+            log.info("  Interpolated:")
+            self.interpolated.dump()
 
+    # doesn't currently support cropped or x-axis
     def load_from_dict(self, d):
         if d is None:
             return
@@ -193,15 +259,49 @@ class ProcessedReading(object):
             "Reference": self.reference,
             "Dark": self.dark,
             "Raw": self.raw,
-            "Processed Cropped": self.processed_cropped,
             "Dark Corrected": self.dark_corrected,
             "Recordable Dark": self.recordable_dark,
             "Recordable Reference": self.recordable_reference,
             "Used Reference": self.used_reference,
             "Raman Intensity Corrected": self.raman_intensity_corrected,
             "Deconvolved": self.deconvolved,
-            "Wavelengths Cropped": self.wavelengths_cropped,
-            "Wavenumbers Cropped": self.wavenumbers_cropped,
             "First Pixel": self.first_pixel,
-            "Plugin Metadata": self.plugin_metadata
+            "Plugin Metadata": self.plugin_metadata,
+            "Wavelengths": self.wavelengths,
+            "Wavenumbers": self.wavenumbers,
+            "Cropped": self.cropped.to_dict() if self.cropped else None
         }
+
+    # The thought here is that ProcessedReading could retain a dict[OoO] of
+    # Readings, where each could retain a unique copy of (processed, dark, 
+    # reference, wavelengths, wavenubers). If a given transform didn't change a 
+    # particular array for a particular measurement, just leave that None.
+    # 
+    # Each array would support a getter with the intended OoO level, and
+    # would traverse DOWN the stack from the requested level to reach the first 
+    # non-None array.
+    #
+    # ENLIGHTEN would allow users to save any array at any level.
+    ORDER_OF_OPERATIONS = [ 
+        "RAW",
+        "EVEN_ODD_INGAAS_CORRECTION",
+        "STOMP_ARRAY_ENDS",
+        "INVERT_X_AXIS",
+        "BAD_PIXEL_CORRECTION",
+        "BIN_2X2",
+        "DARK_CORRECTION",
+        "HORIZ_ROI",
+        "REFERENCE_PROCESSING",
+        "RAMAN_INTENSITY_CORRECTION",
+        "BASELINE_CORRECTION",
+        "PEAK_SHARPENING",
+        "DESPIKING",
+        "BOXCAR",
+        "FINAL" ]
+    ARRAYS = { "PROCESSED", "DARK", "REFERENCE", "WAVELENGTHS", "WAVENUMBERS" }
+    def get_array(self, array_name, level_name):
+        if level_name.upper() not in self.ORDER_OF_OPERATIONS:
+            return
+        if array.upper() not in self.ARRAYS:
+            return
+        pass
