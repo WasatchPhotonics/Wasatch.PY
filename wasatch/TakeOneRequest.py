2a17a622291c87fa22081d61b5d1a1d09bdf0abf
Mark Zieg <mzieg@wasatchphotonics.com>
2024-12-13 17:04:41 -0500

    tentatively looks like it might work?

--- wasatch/TakeOneRequest.py
+++ wasatch/TakeOneRequest.py
@@ -11,7 +11,12 @@ class TakeOneRequest:
                        laser_warmup_ms=0, 
                        scans_to_average=1, 
                        auto_raman_request=None,
-                       template=None):
+                       template=None,
+
+                       # added to let TakeOneRequest represent a streaming source of "many" (but not infinite) Readings
+                       readings_target=None,
+                       readings_current=None):
+
         if template is not None:
             self.take_dark           = template.take_dark
             self.enable_laser_before = template.enable_laser_before
@@ -19,6 +24,10 @@ class TakeOneRequest:
             self.laser_warmup_ms     = template.laser_warmup_ms
             self.scans_to_average    = template.scans_to_average
             self.auto_raman_request  = template.auto_raman_request
+
+            # Not using readings_target/current with templates -- templates are 
+            # specifically for a large BatchCollection where multiple individual 
+            # TakeOneRequests are being manually generated and tracked.
         else:
             self.take_dark           = take_dark
             self.enable_laser_before = enable_laser_before
@@ -26,12 +35,13 @@ class TakeOneRequest:
             self.laser_warmup_ms     = laser_warmup_ms
             self.scans_to_average    = scans_to_average
             self.auto_raman_request  = auto_raman_request
+            self.readings_target     = readings_target
+            self.readings_current    = readings_current
 
         self.request_id = f"{os.getpid():05d}-{threading.get_native_id():05d}-{time.time_ns()}-{random.randrange(65536):05d}"
 
     def __repr__(self):
-        return f"TakeOneRequest <id {self.request_id}, avg {self.scans_to_average}, take_dark {self.take_dark}, enable_laser_before {self.enable_laser_before}, disable_laser_after {self.disable_laser_after}, laser_warmup_ms {self.laser_warmup_ms}, auto_raman_request {self.auto_raman_request}>"
+        return f"TakeOneRequest <id {self.request_id}, avg {self.scans_to_average}, take_dark {self.take_dark}, enable_laser_before {self.enable_laser_before}, disable_laser_after {self.disable_laser_after}, laser_warmup_ms {self.laser_warmup_ms}, readings {self.readings_current}/{self.readings_target}, auto_raman_request {self.auto_raman_request}>"
 
     def __eq__(self, rhs):
         return self.request_id == rhs.request_id
-
