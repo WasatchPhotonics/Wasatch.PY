f597cbfd7a1e464fe645f72cbb234c08019f3b29
Mark Zieg <mzieg@wasatchphotonics.com>
2026-05-28 12:31:08 -0400

    docs

--- wasatch/SpectrometerResponse.py
+++ wasatch/SpectrometerResponse.py
@@ -1,5 +1,5 @@
-from typing import Any
 from enum import Enum
+from typing import Any
 from dataclasses import dataclass
 
 class ErrorLevel(Enum):
@@ -10,23 +10,22 @@ class ErrorLevel(Enum):
 
 @dataclass
 class SpectrometerResponse:
+    """
+    @see docs for SpectrometerRequest
+    """
     data:        Any  = None
     error_msg:   str  = ''
     error_lvl:   int  = ErrorLevel.ok
-    poison_pill: bool = False
     keep_alive:  bool = False
-    incomplete:  bool = False
-    progress:    int  = 0
+    poison_pill: bool = False
 
     def transfer_response(self, rhs):
         """ copy-ctor """
         self.data        = rhs.data
         self.error_msg   = rhs.error_msg
         self.error_lvl   = rhs.error_lvl
-        self.poison_pill = rhs.poison_pill
         self.keep_alive  = rhs.keep_alive
-        self.incomplete  = rhs.incomplete
-        self.progress    = rhs.progress
+        self.poison_pill = rhs.poison_pill
 
     def __repr__(self):
         s = str(self.data)
@@ -38,5 +37,5 @@ class SpectrometerResponse:
         self.data = None
         self.error_msg = ''
         self.error_lvl = ErrorLevel.ok
-        self.poison_pill = False
         self.keep_alive = False
+        self.poison_pill = False
